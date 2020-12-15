d0aa62463abca81f980b85874212b9da700b8013
soohyun99 <yeo7764@gmail.com>
2020-12-15 21:58:33 +0900

    (~session6)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -2,10 +2,10 @@ package hello.core;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 
 public class AutoAppConfig {
-    exclude 
 }
