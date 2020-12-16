62f44c682517ebcf102e4c94d7f7d03908a59590
soohyun99 <yeo7764@gmail.com>
2020-12-16 21:56:53 +0900

    (~session7)

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,6 @@
+package hello.core.annotation;
+
+import java.lang.annotation.*;
+
+public class MainDiscountPolicy {
+}
---
+++ src/test/java/hello/core/autowired/AutoWiredTest.java
@@ -0,0 +1,10 @@
+package hello.core.autowired;
+
+import org.junit.jupiter.api.Test;
+
+public class AutoWiredTest {
+    @Test
+    void AutowiredOption(){
+
+    }
+}
